06a0e6236893d871e598a8e5aa780d55cf80c122
TungNguyen12 <nvtung382@gamil.com>
2023-06-15 09:06:51 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -18,65 +18,31 @@ const App = () => {
 
     const [selected, setSelected] = useState(0);
     const [points, setPoints] = useState(Array(8).fill(0));
-    // We need to update the selected as index and update the array points without mutation
+    // Update the selected as index and update the array points (without mutation)
 
     const copyPoints = [...points];
-    // console.log(copyPoints);
 
     const handleNextClick = () => {
-        // console.log(points);
-        // const newSelected = selected + 1;
-        // setSelected(newSelected)
-        // console.log(newSelected);
         if (selected === ancedotes.length - 1) return;
         const newSelected = selected + 1;
         setSelected(newSelected); // TO UPDATE SELECTED = newSelected
-        // console.log(newSelected, points[newSelected])
-
-        // console.log(selected); //because update State is asynchronous => update selected value by new constant
-        // setSelected(selected + 1)
-        // console.log(selected);
     };
 
     const handleBackClick = () => {
         if (selected === 0) return;
         const newSelected = selected - 1;
         setSelected(newSelected);
-        // console.log(newSelected, points[newSelected])
     };
-    // const copy = [...points]
-    // increment the value in position 2 by one
-    // copy[2] += 10
-    // copy[1] += 21
-    // copy[0] += 13
-    // console.log(copy);
 
     const handleVoteClick = () => {
         copyPoints[selected] += 1;
         setPoints(copyPoints);
-        // console.log(points);
-        // console.log(selected, points[selected]);
-
-        // setSelected(selected)
-        // console.log(selected);
-        // copy[selected] += 1
-        // console.log(copy);
     };
-
-    // const MostVotes = () => {
-    //     if (points.reduce((acc, cur) => acc + cur, 0))
-    //         return (
-    //             ancedotes[points.indexOf(Math.max(...points))]
-    //             <p>Has {Math.max(...points)} votes</p>
-    //             )
-    //     else return <p>Vote to see the most voted quote</p>;
-    // };
 
     return (
         <div>
             <h1>Anecdote of the day</h1>
             {ancedotes[selected]}
-            {/* {console.log(selected, ancedotes.length)} */}
             <br />
             <p>Total votes: {points[selected]} </p>
             <Button handleClick={handleVoteClick} text="Vote" />
@@ -85,8 +51,12 @@ const App = () => {
 
             <h1>Anecdote with most votes</h1>
 
-            
-            {(points.reduce((acc, cur) => acc + cur, 0)) ? ancedotes[points.indexOf(Math.max(...points))] +  ` Has ` + Math.max(...points) +" votes" : "Please vote to see the most voted"}
+            {points.reduce((acc, cur) => acc + cur, 0)
+                ? ancedotes[points.indexOf(Math.max(...points))] +
+                  ` Has ` +
+                  Math.max(...points) +
+                  " votes"
+                : "Please vote to see the most voted"}
         </div>
     );
 };
